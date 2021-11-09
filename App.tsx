a3d1379ea700ae38eb1a0865ec7b6784ed0a073c
WiliamRamirez <wiliam.unsch@gmail.com>
2021-11-09 13:51:54 -0500

    inital commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   const [outputText, setOutputText] = React.useState(
+      'Open up App.tsx to start working on your app!'
+   );
+   return (
+      <View style={styles.container}>
+         <Text>{outputText}</Text>
+         <Button title={'Cambiar texto'} onPress={() => setOutputText('Texto cambiado')} />
+         <StatusBar style='auto' />
+      </View>
+   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+   container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+   },
 });
